a81ce6d185c744cf0c56944ccd37dac54d3e409d
wbkhalid <waleedbinkhalidch@gmail.com>
2024-09-25 16:16:05 +0500

    initial set up for frontend and backend

---
+++ server/index.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+var cookieParser = require('cookie-parser')
+const connectDB = require('./db/db.js')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma"
+    ],
+    credentials: true
+}))
+
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGODB_URL);
+      app.listen(PORT, () => {
+        console.log(`connect at port ${PORT}`);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  start();
